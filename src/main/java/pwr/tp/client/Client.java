e9149a594c336838100ab2fe35875fd6a7222a45
Tomasz N <troll.yuck@gmail.com>
2024-12-04 20:06:26 +0100

    Clients can connect to a server. Server now broadcasts info sent by one Client to all of them, including the sender.

--- src/main/java/pwr/tp/client/Client.java
+++ src/main/java/pwr/tp/client/Client.java
@@ -6,16 +6,69 @@ import java.net.Socket;
 import java.util.Scanner;
 
 public class Client {
-  private final Socket socket;
+  private Socket socket;
   private ObjectOutputStream objectOutputStream;
   private ObjectInputStream objectInputStream;
   private Scanner scannerIn;
+  private boolean running = false;
 
-    public Client(String host, int port) throws Exception {
-        socket = new Socket(host, port);
-        objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
-        objectOutputStream.flush();
-        objectInputStream = new ObjectInputStream(socket.getInputStream());
-        scannerIn = new Scanner(System.in);
+  public Client(String host, int port) throws Exception {
+    try {
+      connect(host, port);
+      Thread receiveThread = new Thread(this::receiveMessages);
+      receiveThread.start();
+      Thread userInputThread = new Thread(this::handleUserInput);
+      userInputThread.start();
+    } catch (Exception e) {
+      e.printStackTrace();
     }
+  }
+
+  private void handleUserInput() {
+    while (running) {
+      String message = scannerIn.nextLine();
+      try {
+        objectOutputStream.writeObject(message);
+        objectOutputStream.flush();
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+    }
+  }
+
+  private void receiveMessages() {
+      try {
+      while (running) {
+          String message = (String) objectInputStream.readObject();
+          System.out.println("Server: " + message);
+      }
+      } catch (Exception e) {
+      e.printStackTrace();
+      }
+  }
+
+  private void connect(String host, int port) {
+      try {
+          socket = new Socket(host, port);
+          objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
+          objectOutputStream.flush();
+          objectInputStream = new ObjectInputStream(socket.getInputStream());
+          scannerIn = new Scanner(System.in);
+          System.out.println("Connected to server on port " + port);
+          running = true;
+      } catch (Exception e) {
+          e.printStackTrace();
+      }
+
+  }
+
+  public static void main(String[] args) {
+      try {
+          new Client("localhost", 12345);
+      } catch (Exception e) {
+          e.printStackTrace();
+      }
+  }
+
+
 }
--- src/main/java/pwr/tp/server/ClientHandler.java
+++ src/main/java/pwr/tp/server/ClientHandler.java
@@ -1,19 +1,48 @@
 package pwr.tp.server;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.Socket;
+
 
 public class ClientHandler implements Runnable {
 
   private Socket clientSocket;
   private GameHostServer gameHostServer;
+  private ObjectOutputStream out;
+  private ObjectInputStream in;
 
     public ClientHandler(Socket clientSocket, GameHostServer gameHostServer) {
         this.clientSocket = clientSocket;
         this.gameHostServer = gameHostServer;
+        try {
+            out = new ObjectOutputStream(clientSocket.getOutputStream());
+            out.flush();
+            in = new ObjectInputStream(clientSocket.getInputStream());
+        } catch (IOException ioe) {
+          ioe.printStackTrace();
+        }
     }
 
   @Override
   public void run() {
+    try {
+      while (true) {
+        Object object = in.readObject();
+        gameHostServer.broadcast(object);
+      }
+    } catch (IOException | ClassNotFoundException e) {
+      gameHostServer.removeClient(this);
+    }
+  }
 
+  public void send(Object object) {
+    try {
+      out.writeObject(object);
+      out.flush();
+    } catch (IOException ioe) {
+      ioe.printStackTrace();
+    }
   }
 }
