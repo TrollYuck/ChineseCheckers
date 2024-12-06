95791c4a3d3400a6e81c9cb6bd55184dd24cfd1d
Karol Wziatek <wziatek.karol04@gmail.com>
2024-12-07 00:21:55 +0100

    change class name to CreateLobby

--- src/main/java/pwr/tp/game/CreateLobby.java
+++ src/main/java/pwr/tp/game/CreateLobby.java
@@ -1,20 +1,17 @@
 package pwr.tp.game;
 
 import pwr.tp.domain.Board;
-import pwr.tp.domain.StarBoard.NoPlayersStarBoard;
 import pwr.tp.domain.StarBoard.StarBoard;
 
-public class CreateGame {
+public class CreateLobby {
 
-    public static Lobby createGame(int numOfPlayers, String boardTypeString) throws IllegalBoardTypeException, IllegalNumberOfPlayersException{
+    public static Lobby createLobby(int numOfPlayers, String boardTypeString) throws IllegalBoardTypeException, IllegalNumberOfPlayersException{
         if(boardTypeString.equalsIgnoreCase("star board")) {
-            for(NoPlayersStarBoard noPlayersStarBoard: NoPlayersStarBoard.values()) {
                 Board board = new StarBoard();
-
                 if(board.getPossibleNoPlayers().contains(numOfPlayers)) {
                     return new Lobby(numOfPlayers, new StarBoard());
                 }
-            }
+
             throw new IllegalNumberOfPlayersException();
         }
         throw new IllegalBoardTypeException();
